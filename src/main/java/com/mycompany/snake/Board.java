67658cdb6000ebfbb1299b777700a8cd6e10928f
alu13257670 <alu13257670@ti2-03>
2024-05-17 19:10:32 +0200

    Working on using ConfigDialog for setting also to restart game.

--- src/main/java/com/mycompany/snake/Board.java
+++ src/main/java/com/mycompany/snake/Board.java
@@ -4,17 +4,197 @@
  */
 package com.mycompany.snake;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.Timer;
+
 /**
  *
  * @author alu13257670
  */
 public class Board extends javax.swing.JPanel {
 
+    class MyKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_UP:
+                    snake.setDirection(Direction.UP);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    snake.setDirection(Direction.DOWN);
+                    break;
+                case KeyEvent.VK_LEFT:
+                    snake.setDirection(Direction.LEFT);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    snake.setDirection(Direction.RIGHT);
+                    break;
+
+                case KeyEvent.VK_SPACE:
+                    pauseGame();
+                    break;
+                default:
+                    System.out.println("Hello Snake");
+                    break;
+            }
+            //repaint();
+        }
+    }
+
+    public static final int NUM_ROWS = 30;
+    public static final int NUM_COLS = 30;
+
+    private Snake snake;
+    private Food food;
+    private SpecialFood sFood;
+    private int currentRow;
+    private int currentCol;
+    private Timer timer;
+    private int catchSPCount = 0;
+    private static final int SP_FOOD_CATCH_INTERVAL = 40;
+    private int respawnSPCount = 0;
+    private static final int SP_FOOD_RESPAWN_INTERVAL = 100;
+    private MyKeyAdapter keyAdapter;
+    
+    private Game game;
+
     /**
      * Creates new form Board
      */
     public Board() {
         initComponents();
+    }
+    
+    public Board(Game game) {
+        this.game = game;
+        //initComponents();
+        newGame();
+        /*snake = new Snake();
+        food = genFood();
+        sFood = genSpecialFood();
+        keyAdapter = new MyKeyAdapter();*/
+        //initGame();
+        //repaint();
+    }
+
+    public void initGame() {
+        addKeyListener(keyAdapter);
+        setFocusable(true);
+        //setRequestFocusEnabled(true);
+        int deltaTime = ConfigData.getInstance().getDeltaTime();
+        if (timer != null && timer.isRunning()) {
+            timer.stop();
+        }
+        timer = new Timer(deltaTime, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                tick();
+            }
+
+        });
+        timer.start();
+        //setRequestFocusEnabled(true);
+        if (!snake.move()) {
+            Game game = new Game();
+            newGame();
+            game.initSettingsDialog();
+            
+        };
+    }
+
+    public void pauseGame() {
+        if (timer.isRunning()) {
+            timer.stop();
+        } else {
+            timer.start();
+        }
+    }
+
+    public void newGame() {
+        snake = new Snake();
+        food = genFood();
+        sFood = genSpecialFood();
+        //sFood=null;
+        keyAdapter = new MyKeyAdapter();
+        requestFocus();
+
+        initGame();
+        repaint();
+    }
+
+    private void tick() {
+        /*if (catchSPCount == SP_FOOD_CATCH_INTERVAL) {
+            if (respawnSPCount == SP_FOOD_RESPAWN_INTERVAL) {
+                sFood = genSpecialFood();
+                respawnSPCount = 0;
+            }
+            sFood = null;
+            catchSPCount = 0;
+        }*/
+
+        if (snake.findsFood(food)) {
+            //snake.getBody().add(food);
+            snake.setNodesToGrow(1);
+            food = genFood();
+        } else {
+            if (snake.findsFood(sFood)) {
+                //snake.getBody().add(sFood);
+                snake.setNodesToGrow(3);
+                //sFood = genSpecialFood();
+                if (respawnSPCount == SP_FOOD_RESPAWN_INTERVAL) {
+                    sFood = genSpecialFood();
+                    respawnSPCount = 0;
+                }
+            }
+            if (catchSPCount == SP_FOOD_CATCH_INTERVAL) {
+                catchSPCount = 0;
+            }
+
+        }
+        
+        repaint();
+        catchSPCount++;
+        respawnSPCount++;
+    }
+
+    public Food genFood() {
+        return new Food((int) (Math.random() * NUM_ROWS), (int) (Math.random() * NUM_COLS));
+    }
+
+    public SpecialFood genSpecialFood() {
+        return new SpecialFood((int) (Math.random() * NUM_ROWS), (int) (Math.random() * NUM_COLS));
+    }
+
+    public int getSquareWidth() {
+        return getWidth() / NUM_COLS;
+        //return getWidth() / ConfigData.getInstance().getNumCols();
+    }
+
+    public int getSquareHeight() {
+        return getHeight() / NUM_ROWS;
+        //return getHeight() / ConfigData.getInstance().getNumRows();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        //g.fillRect(0, 0, getWidth(), getHeight());
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.drawRect(0, 0, getWidth() / NUM_COLS * NUM_COLS,
+                getHeight() / NUM_ROWS * NUM_ROWS);
+        if (!snake.getBody().isEmpty()) {
+            snake.paint(g, getSquareWidth(), getSquareHeight());
+            food.paint(g, getSquareWidth(), getSquareHeight());
+            //sFood.paint(g, getSquareWidth(), getSquareHeight());
+        }
+        Toolkit.getDefaultToolkit().sync();
     }
 
     /**
@@ -26,7 +206,9 @@ public class Board extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setLayout(new java.awt.GridLayout());
+        setBackground(new java.awt.Color(255, 255, 255));
+        setForeground(new java.awt.Color(255, 255, 255));
+        setLayout(new java.awt.GridLayout(1, 0));
     }// </editor-fold>//GEN-END:initComponents
 
 
