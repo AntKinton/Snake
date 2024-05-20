0fbee16ea43f20899832221194c41b930b1cb137
alu13257670 <alu13257670@ti2-03>
2024-05-20 20:40:48 +0200

    Node null pointer exceptions now are contempled into findsFood() method.

--- src/main/java/com/mycompany/snake/Snake.java
+++ src/main/java/com/mycompany/snake/Snake.java
@@ -5,106 +5,120 @@
 package com.mycompany.snake;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author alu13257670
  */
 public class Snake {
+
     private List<Node> body;
     private Direction direction;
     private int nodesToGrow;
-    
-    
+
     public Snake() {
         body = new ArrayList<Node>();
-        body.add(new Node(Board.NUM_ROWS/2, Board.NUM_COLS/2));
-        body.add(new Node(Board.NUM_ROWS/2, Board.NUM_COLS/2 - 1));
-        body.add(new Node(Board.NUM_ROWS/2, Board.NUM_COLS/2 - 2));
-        body.add(new Node(Board.NUM_ROWS/2, Board.NUM_COLS/2 - 3));
+        body.add(new Node(Board.NUM_ROWS / 2, Board.NUM_COLS / 2));
+        body.add(new Node(Board.NUM_ROWS / 2, Board.NUM_COLS / 2 - 1));
+        body.add(new Node(Board.NUM_ROWS / 2, Board.NUM_COLS / 2 - 2));
+        body.add(new Node(Board.NUM_ROWS / 2, Board.NUM_COLS / 2 - 3));
         direction = Direction.RIGHT;
         nodesToGrow = 0;
     }
-    
+
     public List<Node> getBody() {
         return body;
     }
-    
-    
+
     public Node getHeadNode() {
         return body.get(0);
     }
-    
+
     public Node getTailNode() {
-        return body.get(body.size()-1);
+        return body.get(body.size() - 1);
     }
-    
+
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
-    
-    public void move() {
+
+    public boolean makeMove() {
         int headRow = getHeadNode().getRow();
         int headCol = getHeadNode().getCol();
-       
+
         switch (direction) {
-            case UP: headRow--;
+            case UP:
+                headRow--;
                 break;
-            case DOWN: headRow++;
+            case DOWN:
+                headRow++;
                 break;
-            case LEFT: headCol--;
+            case LEFT:
+                headCol--;
                 break;
-            case RIGHT: headCol++;
+            case RIGHT:
+                headCol++;
                 break;
         }
-        if (canMove(headRow, headCol)) {
-            Node node = new Node (headRow,headCol);
-            while (nodesToGrow > 0) {
-                body.add(0, node);
+        if (checkCollision(headRow, headCol)) {
+            Node node = new Node(headRow, headCol);
+            if (nodesToGrow > 0) {
                 nodesToGrow--;
+            } else {
+                removeTailNode();
             }
-            System.out.println(body.size());
-            removeTailNode();
-        }
-    }
-    
-    private void removeTailNode(){
-         if (nodesToGrow == 0 && !body.isEmpty()){
-           
-            body.remove(getTailNode());
-        }
-        System.out.println(body.size());
-    }
-    
-    public boolean canMove(int headRow, int headCol) {
-        if (headRow < 0 || headRow >= Board.NUM_ROWS) {
-            return false;
-            
+            body.add(0, node);
+            return true;
         } else {
-            if (headCol < 0 || headCol >= Board.NUM_COLS) {
+            return false;
+        }
+    }
+
+    public boolean checkCollision(int headRow, int headCol) {
+        if (headRow < 0 || headRow >= Board.NUM_ROWS || headCol < 0 || headCol >= Board.NUM_COLS) {
+            return false;
+        }
+
+        for (Node node : body) {
+            if (node.getRow() == headRow && node.getCol() == headCol) {
                 return false;
             }
         }
         return true;
-        
     }
-    
-    public boolean findsFood(Node node) {
-        return (getHeadNode().row == node.row 
-                && getHeadNode().col == node.col);
+
+    private void removeTailNode() {
+        if (nodesToGrow == 0 && !body.isEmpty()) {
+            body.remove(getTailNode());
+        }
+        //System.out.println(body.size());
     }
-    
+
+   public boolean findsFood(Node node) {
+        Node head = getHeadNode();
+        if (head == null) {
+            //System.out.println("Head node is null.");
+            return false;
+        }
+
+        if (node == null) {
+            //System.out.println("Food node is null.");
+            return false;
+        }
+
+        return head.getRow() == node.getRow() && head.getCol() == node.getCol();
+    }
+
     public void setNodesToGrow(int nodesToGrow) {
         this.nodesToGrow = nodesToGrow;
     }
-    
-    public void paint (Graphics g, int squareWidth, int squareHeight) {
+
+    public void paintBody(Graphics g, int squareWidth, int squareHeight) {
         boolean firstNode = true;
         Color color;
-        for (Node node: body) {
+        for (Node node : body) {
             if (firstNode) {
                 color = Color.green;
                 firstNode = false;
@@ -114,7 +128,5 @@ public class Snake {
             Util.drawSquare(g, node.getRow(), node.getCol(), color, squareWidth, squareHeight);
         }
     }
-    
-    
-    
+
 }
